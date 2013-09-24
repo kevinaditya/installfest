5ffabcdd3bb0791205d0dcf580b3db659c9ec707
Kevin aditya <k_vin92@yahoo.com>
2013-09-24 20:03:18 +1000

    initial blog commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickBlog::Application.config.secret_token = '48bde506a568bc68e77b7b6b3dac1b91f003044b30a4a9e4d9e1b122b2a016e376cabbad50b85343bb9ed9da538760b9d17481f4b8d0babaa53b3db908cd4d2e'
